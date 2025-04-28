d35a1c89d83c0be479b367346b3d22a89fed435d
Sergio Restrepo Prado <1112040337@u.icesi.edu.co>
2025-04-28 09:40:50 -0500

    guarda

--- client-app1/PantallaCarga/Loading.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    const loadingScreen = document.getElementById('loading-screen');
-
-    setTimeout(() => {
-        loadingScreen.style.display = 'none';
-    }, 3000); // 3 seconds delay
-}
